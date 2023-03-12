a49c6161c84507ff6409e9d0adb8960a675338f4
Boris Balaban <boris.balaban@devexpress.com>
2023-03-12 13:28:59 +0400

    tailwind is removed.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {Panel} from './components/panel'
 
 function App() {
   return (
-    <body className="h-screen bg-gradient-to-b from-gray-100 to-gray-300">
-      <div className="container mx-auto max-w-6xl pt-5">
+    <body className='main'>
+      <div className='content'>
         <Panel />
       </div>
     </body>
--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function Panel() {
     return (
-        <div className='h-40 rounded-bl-lg rounded-bl-3xl bg-white drop-shadow-xl'>
+        <div className='panel'>
             Panel
         </div>
     )
