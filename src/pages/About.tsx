5f1ba79d4a90ba04bc232361e59c94e32cebce45
Boris Balaban <boris.balaban@devexpress.com>
2023-04-09 02:13:02 +0400

    The buy page is added

---
+++ src/pages/About.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Product } from '../components/product'
+import { NavBar } from '../components/navbar'
+import { Panel } from '../components/panel'
+import { FormoJobPanel } from '../components/formojob_panel'
+import { ExtensionPanel } from '../components/extension_panel'
+import { APIPanel } from '../components/API_panel'
+
+export function About() {
+  return (
+    <div className='content'>
+      <link href="https://fonts.googleapis.com/css?family=Raleway:400,700,900&display=swap" rel="stylesheet"></link>
+      <NavBar />
+      <Product panel={
+        <Panel 
+          theme='lightPanel' 
+          headerText='A forms OCR auto-filling tool' 
+          headerDescription='You talk to your client over a cup of coffee - we generate your contracts' 
+          children={ <FormoJobPanel />} />
+        } 
+        buttonText='Try Designer' 
+        link='.' />
+
+      <Product panel={
+        <Panel 
+          theme='lightPanel' 
+          headerText='Browser Extension'
+          headerDescription='Your browser form-filling assistant'
+          children={ <ExtensionPanel />} />
+        } 
+        buttonText='Try Extension' 
+        link='.' />
+
+      <Product panel={
+        <Panel 
+          theme='darkPanel' 
+          headerText='API'
+          headerDescription='Create your own applications based on our recognizing interface'
+          children={ <APIPanel />} />
+        } 
+        buttonText='GitHub' 
+        link='.' />
+    </div>
+  );
+}
---
+++ src/pages/Buy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavBar } from '../components/navbar'
+import Data from "../data/BuyPage.json"
+
+export function Buy() {
+  return (
+    <div className='content'>
+      <link href="https://fonts.googleapis.com/css?family=Raleway:400,700,900&display=swap" rel="stylesheet"></link>
+      <NavBar />
+    </div>
+  );
+}
